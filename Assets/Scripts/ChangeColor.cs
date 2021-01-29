856d8bdb7aa9e48b22acdefc7993ba654299f87b
DESKTOP-5JNU07B\Sean <sean.silla@gmail.com>
2021-01-29 09:56:21 +0100

    Now it changes color over time

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public Renderer _renderer;
+    
+     public float timer = 0.0f;
+
+ 
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 1.0f)//change the float value here to change how long it takes to switch.
+        {
+            // pick a random color
+            Color newColor = new Color(Random.value, Random.value, Random.value, 1.0f);
+            // apply it on current object's material
+            _renderer.material.color = newColor;
+            timer = 0;
+        }
+
+
+    }
+
+    //TEST
+}
+
+ 
+
